140a5689fd53ae959b0cfd73e239062553bc7bf6
catlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2021-10-18 14:37:37 +0900

    서버 업데이트

--- app/src/main/java/com/example/capture/video/game/MainActivity.java
+++ app/src/main/java/com/example/capture/video/game/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView main_img = (ImageView)findViewById(R.id.imageView);
+        Glide.with(this).load(R.raw.main).into(main_img);
 
+        Button imageButton = (Button) findViewById(R.id.game3_btn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Character.class);
+                startActivity(intent);
+            }
+        });
     }
 }
