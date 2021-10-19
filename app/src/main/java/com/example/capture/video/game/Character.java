b2bbba4ff4d960b5410fc08cb50fc12fb6e660e9
catlejuyeon <87415326+catlejuyeon@users.noreply.github.com>
2021-10-19 16:02:09 +0900

    이미지 업로드

--- app/src/main/java/com/example/capture/video/game/Character.java
+++ app/src/main/java/com/example/capture/video/game/Character.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,6 +18,28 @@ public class Character extends AppCompatActivity {
         setContentView(R.layout.character);
 
         TextView level=findViewById(R.id.level);
+        ImageView exp_bar=findViewById(R.id.exp_bar);
+
+        ImageView emoji5=findViewById(R.id.emoji5);
+        ImageView emoji10=findViewById(R.id.emoji10);
+        ImageView emoji15=findViewById(R.id.emoji15);
+        ImageView emoji20=findViewById(R.id.emoji20);
+        ImageView emoji25=findViewById(R.id.emoji25);
+        ImageView emoji30=findViewById(R.id.emoji30);
+
+        ImageView hat5=findViewById(R.id.hat5);
+        ImageView hat10=findViewById(R.id.hat10);
+        ImageView hat15=findViewById(R.id.hat15);
+        ImageView hat20=findViewById(R.id.hat20);
+        ImageView hat25=findViewById(R.id.hat25);
+        ImageView hat30=findViewById(R.id.hat30);
+
+        ImageView clothes5=findViewById(R.id.clothes5);
+        ImageView clothes10=findViewById(R.id.clothes10);
+        ImageView clothes15=findViewById(R.id.clothes15);
+        ImageView clothes20=findViewById(R.id.clothes20);
+        ImageView clothes25=findViewById(R.id.clothes25);
+        ImageView clothes30=findViewById(R.id.clothes30);
 
         level.setText("Lv"+"30");
 
@@ -28,6 +51,7 @@ public class Character extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
     }
 }
