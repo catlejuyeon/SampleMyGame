a27e59cf5085b774b8f6f0420670a40db3f2941e
poning6 <90896491+poning6@users.noreply.github.com>
2021-11-15 00:19:53 +0900

    팝업

--- app/src/main/java/com/example/capture/video/game/Game1.java
+++ app/src/main/java/com/example/capture/video/game/Game1.java
@@ -1,12 +1,15 @@
 package com.example.capture.video.game;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Timer;
@@ -14,6 +17,8 @@ import java.util.TimerTask;
 
 public class Game1 extends AppCompatActivity {
 
+    Button btn_hint;
+    Button btn_stop;
     TextView TextView;
     TimerTask timerTask;
     Timer timer = new Timer();
@@ -24,6 +29,22 @@ public class Game1 extends AppCompatActivity {
         setContentView(R.layout.game1);
 
         TextView = findViewById(R.id.Time);
+        btn_hint = findViewById(R.id.Hint);
+        btn_stop = findViewById(R.id.Stop);
+
+        btn_hint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog2();
+            }
+        });
 
     }
 
@@ -38,6 +59,9 @@ public class Game1 extends AppCompatActivity {
     {
         switch(view.getId())
         {
+            case R.id.Start:
+                startTimerTask();
+                break;
             case R.id.Stop:
                 stopTimerTask();
                 break;
@@ -50,7 +74,7 @@ public class Game1 extends AppCompatActivity {
 
         timerTask = new TimerTask()
         {
-            int count = 60;
+            int count = 30;
 
             @Override
             public void run()
@@ -76,4 +100,40 @@ public class Game1 extends AppCompatActivity {
             timerTask = null;
         }
     }
+
+    void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Game1.this);
+            builder.setIcon(R.drawable.bt_hint);
+            builder.setTitle("힌트");
+            builder.setMessage("두 개의 카드를 뒤집어 같은 그림이 나오도록 하세요");
+            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+        builder.show();
+    }
+
+    void showDialog2() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Game1.this);
+            builder.setIcon(R.drawable.bt_stop);
+            builder.setTitle("일시정지");
+
+            builder.setPositiveButton("다시하기", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNegativeButton("나가기", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+    }
+
 }
